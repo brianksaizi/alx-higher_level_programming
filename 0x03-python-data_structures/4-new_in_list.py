36413849ad41d6b2d6d6fe8ac39533460bd34596
brianksaizi <saizibk@gmail.com>
2023-08-14 20:01:08 -0700

    update

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
-# 4-new_in_list.py
-
 
 def new_in_list(my_list, idx, element):
-    """Substitute an element within a duplicated list at a particular position."""
-    if idx < 0 or idx > (len(my_list) - 1):
-        return (my_list)
+    if idx < 0 or idx >= len(my_list):
+        return my_list.copy()
 
-    copy = [x for x in my_list]
-    copy[idx] = element
-    return (copy)
+    new_list = my_list.copy()
+    new_list[idx] = element
+    return new_list
+
+# Test cases
+original_list = [1, 2, 3, 4, 5]
+idx = 2
+new_element = 10
+
+modified_list = new_in_list(original_list, idx, new_element)
+print("Original List:", original_list)
+print("Modified List:", modified_list)
