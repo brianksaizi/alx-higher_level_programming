94f9af3c5543d4e80ac83063ea074dfca5e2441c
brianksaizi <saizibk@gmail.com>
2023-08-14 20:12:39 -0700

    u

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,18 +1,12 @@
 #!/usr/bin/python3
+# 4-new_in_list.py
+
 
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
-        return my_list[:]
+    """Swap an element in a cloned list at a given position."""
+    if idx < 0 or idx > (len(my_list) - 1):
+        return (my_list)
 
-    new_list = my_list[:]
-    new_list[idx] = element
-    return new_list
-
-# Test cases
-original_list = [1, 2, 3, 4, 5]
-idx = 2
-new_element = 10
-
-modified_list = new_in_list(original_list, idx, new_element)
-print("Original List:", original_list)
-print("Modified List:", modified_list)
+    copy = [x for x in my_list]
+    copy[idx] = element
+    return (copy)
