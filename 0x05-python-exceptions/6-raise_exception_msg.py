46a6569719c1ec06dbf3600a0035a0e5c0f2fc85
brianksaizi <saizibk@gmail.com>
2023-08-30 07:41:37 -0700

    wow

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-"""Define a function called raise_exception_msg to raise a NameError exception with a custom message."""
+
 
 def raise_exception_msg(message=""):
-    """Raise a NameError exception with a specified message.
-
-    Args:
-        message (str, optional): The custom error message to raise with the exception.
-    """
+    """Generate a NameError with a message."""
     raise NameError(message)
+
